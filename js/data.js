f84d56af0acdd0f7d57c81f385a599c77985e57d
qizhang4 <qizhang4@illinois.edu>
2018-02-05 00:03:23 -0500

    Modify data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,48 @@
 var me = {
     'name': 'Qi Zhang',
-    'major': 'Computer Science'
+    'major': 'Computer Science',
+    'currentReading': [
+        {
+            'name': 'You Don\'t Know JS',
+            'url': 'https://github.com/getify/You-Dont-Know-JS',
+            'authors': [
+                'Bear Bibeault',
+                'John Resig'
+            ]
+        },
+        {
+            'name': 'Secrets of the JavaScript Ninja',
+            'authors': [
+                'Kyle Simpson'
+            ]
+        }
+    ],
+    'willRead': [
+
+    ],
+    'speak': [
+        'Javascript', 'Typescript', 'Python', 'Ruby', 'Java', 'C++', 'C'
+    ],
+    'techStack': [
+        'Angular 2+', 'Node.js', 'Express', 'RxJS'
+
+    ],
+    'videoGames': [
+
+    ],
+    'workExperience': [
+        {
+            'company': 'Comcast',
+            'employeeType': 'Contractor',
+            'title': 'Frontend Developer',
+            'projectDescription': 'Build Angular Application for Xfinity Mobile agents.',
+            'technology': [
+                'Angular 4', 'SCSS', 'CSS', 'HTML5', 'Swagger', 'Postman', 'Karma', 'Jasmine'
+            ]
+        }
+    ],
+    'willLearn': [
+        'D3.js', 'Webpack', 'Echarts', 'React', 'Ngrx'
+    ]
+
 }
